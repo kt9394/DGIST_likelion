5e040c534d1e3ef7c37d7d88cc7c13e561800287
EC2 Default User <ec2-user@ip-172-31-36-213.us-east-2.compute.internal>
2018-08-20 14:11:20 +0000

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-Case.create!(total_asset: 1; marriage_year: 2; separation_period_year: 3; separation_period_month: 4; agreement: 5; parental_status: 6; title: abc; abstract: defqwer; url: qwer;)
+Case.create!(total_asset: 1; marriage_year: 2; separation_period_year: 3; separation_period_month: 4; agreement: 5; parental_status: 6; title: "abc"; abstract: "defqwer"; url: "qwer";)
